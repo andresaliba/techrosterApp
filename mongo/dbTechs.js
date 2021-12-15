9ccf568ea968aad47a144cf77b381e3d6c9a907f
Andre Saliba <andrecstk@hotmail.com>
2021-12-14 23:54:03 -0300

    Database fix

--- mongo/dbTechs.js
+++ mongo/dbTechs.js
@@ -123,3 +123,46 @@ db.technologies.insert([
     }
 ]);
 
+// drop collection if already exists
+db.courses.drop();
+// insert new documents into collection
+db.courses.insert([
+    {
+        "code": "APPD1001",
+        "name": "User Interface Design and Development",
+    },
+    {
+        "code": "INET2005",
+        "name": "Web Application Programming I",
+    },
+    {
+        "code": "PROG1400",
+        "name": "Intro to Object Oriented Programming",
+    },
+    {
+        "code": "PROG1700",
+        "name": "Logic and Programming",
+    },
+    {
+        "code": "PROG2700",
+        "name": "Client Side Programming",
+    },
+    {
+        "code": "PROG3017",
+        "name": "Full Stack Programming",
+    },
+    {
+        "code": "WEBD1000",
+        "name": "Website Development",
+    },
+    {
+        "code": "WEBD3000",
+        "name": "Web Application Programming II",
+    },
+    {
+        "code": "WEBD3027",
+        "name": "Developing for Content Management Systems",
+    }
+]);
+
+
